293e6096e116632c47ef8701b94879497f18ee89
Mark Cortez <markcortez2002@gmail.com>
2022-01-14 00:49:26 +0800

    start break button functionality added

--- app/src/main/java/com/example/mayana/CountdownTimer.java
+++ app/src/main/java/com/example/mayana/CountdownTimer.java
@@ -16,13 +16,18 @@ public class CountdownTimer extends AppCompatActivity {
 
     private EditText inputWorkDuration;
     private EditText inputBreakDuration;
+
     private TextView mTextViewCountDown;
+
     private TextView workDurationLabel;
     private TextView breakDurationLabel;
+
     private Button setWorkingDuration;
     private Button setBreakDuration;
+
     private Button workButton;
     private Button breakButton;
+
     private Button mButtonReset;
 
     private CountDownTimer mCountDownTimer;
@@ -31,6 +36,9 @@ public class CountdownTimer extends AppCompatActivity {
     private long mStartTimeInMillis;
     private long mTimeLeftInMillis = mStartTimeInMillis;
 
+    private long break_millisInput;
+    private long work_millisInput;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,36 +46,73 @@ public class CountdownTimer extends AppCompatActivity {
 
         inputWorkDuration = findViewById(R.id.edit_text_input);
         inputBreakDuration = findViewById(R.id.edit_text_break);
+
         mTextViewCountDown = findViewById(R.id.text_view_countdown);
+
         workDurationLabel = findViewById(R.id.textView_work_label);
         breakDurationLabel = findViewById(R.id.textView_break_label);
+
         setWorkingDuration = findViewById(R.id.button_set);
         setBreakDuration = findViewById(R.id.button_set_break);
+
         workButton = findViewById(R.id.button_start_pause);
         breakButton = findViewById(R.id.button_start_pause_break);
+
         mButtonReset = findViewById(R.id.button_reset);
 
         setWorkingDuration.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String input = inputWorkDuration.getText().toString();
-                if (input.length() == 0){
+
+                String work_duration_input = inputWorkDuration.getText().toString();
+
+                if (work_duration_input.length() == 0){
                     Toast.makeText(CountdownTimer.this, "Please enter desired duration in the minutes field", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
-                long millisInput = Long.parseLong(input) * 60000;
-                if (millisInput == 0) {
+                work_millisInput = Long.parseLong(work_duration_input) * 60000;
+
+                if (work_millisInput == 0) {
                     Toast.makeText(CountdownTimer.this, "Duration must be greater than '0'", Toast.LENGTH_SHORT).show();
+                    return;
+                } else {
+                    Toast.makeText(CountdownTimer.this, "Work duration successfully set.", Toast.LENGTH_SHORT).show();
+                }
+
+                setTime(work_millisInput);
+                inputWorkDuration.setText("");
+                inputBreakDuration.setText("");
+            }
+        });
+
+        setBreakDuration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String break_duration_input = inputBreakDuration.getText().toString();
+
+                if (break_duration_input.length() == 00){
+                    Toast.makeText(CountdownTimer.this, "Please enter desired duration in the minutes field", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
-                setTime(millisInput);
+                break_millisInput = Long.parseLong(break_duration_input) * 60000;
+
+                if (break_millisInput == 0) {
+                    Toast.makeText(CountdownTimer.this, "Duration must be greater than '0'", Toast.LENGTH_SHORT).show();
+                    return;
+                }else {
+                    Toast.makeText(CountdownTimer.this, "Break Duration successfully set.", Toast.LENGTH_SHORT).show();
+                }
+
                 inputWorkDuration.setText("");
+                inputBreakDuration.setText("");
             }
         });
 
         workButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
                 if (mTimerRunning){
@@ -77,6 +122,20 @@ public class CountdownTimer extends AppCompatActivity {
                 }
             }
         });
+
+        breakButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                if (mTimerRunning){
+                    pauseTimer();
+                }else{
+                    setTime(break_millisInput);
+                    startTimer();
+                }
+            }
+        });
+
 
         mButtonReset.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -117,8 +176,8 @@ public class CountdownTimer extends AppCompatActivity {
         setWorkingDuration.setVisibility(View.INVISIBLE);
         setBreakDuration.setVisibility(View.INVISIBLE);
         workButton.setText("Pause Work");
-        breakButton.setVisibility(View.INVISIBLE);
-        mButtonReset.setVisibility(View.INVISIBLE);
+        breakButton.setVisibility(View.VISIBLE);
+        mButtonReset.setVisibility(View.VISIBLE);
     }
     private void pauseTimer(){
         mCountDownTimer.cancel();
